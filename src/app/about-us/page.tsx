a7042788d9ea8503dd19914e6413aab5bfd13d02
jash215 <58883072+jd-215@users.noreply.github.com>
2023-12-03 19:53:00 +0530

    about us page added

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutUsPage() {
+    return (
+      <main >
+        <div>
+          <h1 className="m-6 text-3xl font-bold">About Us Page</h1>
+        </div>
+      </main>
+    )
+  }
