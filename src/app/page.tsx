9945f2b65ead5ed7890c1b7aa13daa79d54d7f4e
jash215 <58883072+jd-215@users.noreply.github.com>
2023-12-05 23:52:56 +0530

    image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 import Image from 'next/image'
 
+const ImageComonent = () => {
+  return (
+    <div>
+      {/* <Image src={"/images/hero-image.png"} style={{position:"static"}} width={1700} height={500}  alt='image' /> */}
+      <Image src={"/logoicon/imageone.svg"} style={{position:"static"}} width={1600} height={500}  alt='image' />
+    </div>
+  )
+} 
+const ImageComonentTwo = () => {
+  return (
+    <div>
+      <Image src={"/logoicon/frametwo.svg"} style={{position:"static"}} width={1700} height={500}  alt='image' />
+    </div>
+  )
+} 
+
 export default async function Home() {
 
   return (
     <main >
-      <div className="bg-cover h-screen">
-		{/* <ImageComonents /> */}
-        {/* <h1 className="m-6 text-3xl font-bold">Hello world </h1> */}
-		{/* <Image src={"/app/logoicon/favicon.ico"} width={700} height={500} alt='image' /> */}
+      <div className="grid">
+		<ImageComonent />  
+    {/* <ImageComonentTwo /> */}
       </div>
     </main>
   )
